38ee9c08e6c9b7a4fb4521e845ddf7a72c47cd5f
heather <heatherlynn1337@gmail.com>
2019-06-27 15:49:38 -0700

    adding three top game images

--- src/topGames.jsx
+++ src/topGames.jsx
@@ -9,12 +9,19 @@ const height = 280;
 const TopGames = (props) => {
   console.log('props --->', props.data);
   var game1 = props.data[0].box_art_url;
-  var gameImg = game1.replace("{height}", "250").replace("{width}", "188");
+  var gameImg1 = game1.replace("{height}", "250").replace("{width}", "188");
+  var game2 = props.data[1].box_art_url;
+  var gameImg2 = game2.replace("{height}", "250").replace("{width}", "188");
+  var game3 = props.data[2].box_art_url;
+  var gameImg3 = game3.replace("{height}", "250").replace("{width}", "188");
+
   console.log(game1);
   return (
     <div>
       <div>Top Games Component</div>  
-      <img src={gameImg}></img>
+      <div><img src={gameImg1}></img></div>
+      <div><img src={gameImg2}></img></div>
+      <div><img src={gameImg3}></img></div>
     </div>
   )
 }
