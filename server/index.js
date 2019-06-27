48e251f170cacd19b406ac0df22704417b577568
heather <heatherlynn1337@gmail.com>
2019-06-26 18:52:32 -0700

    adding initial server setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 2000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
